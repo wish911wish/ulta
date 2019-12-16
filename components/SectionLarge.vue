8008d4b634400ed159b49897f3f0cd33a8b5d985
wish911wish <wish911wish@gmail.com>
2019-12-17 07:51:10 +0900

    make component for section

---
+++ components/SectionLarge.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>
+    {{ name }}
+  </h1>
+</template>
+
+<script>
+
+export default {
+  props: ['name']
+}
+
+</script>
+
+<style scoped>
+h1 {
+  /* Custom Propertiesの設定方法を調べること */
+  /* color: var(--v-primary); */
+  color: #94AA2A;
+}
+</style>

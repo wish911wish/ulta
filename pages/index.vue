a5997147387291792e81fec569c368d697343445
wish911wish <wish911wish@gmail.com>
2019-12-17 22:50:11 +0900

    fix top page component

--- pages/index.vue
+++ pages/index.vue
@@ -8,85 +8,39 @@
       xs12
       sm8
       md6
+      w-100
     >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template. Start markup for user screen
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
+      <div class="contents-container">
+        <h1>ULTA!</h1>
+        <p>“..ultimate frisbee combines</p>
+        <p>speed, grace and powerful hurling</p>
+        <p>with a grueling pace.”</p>
+        <p class="mt-4">THE WALL STREET JOURNAL</p>
+        <div class="text-center">
           <v-btn
             color="primary"
             nuxt
             to="/event"
           >
-            Events
+            Do Ultimate!!
           </v-btn>
-        </v-card-actions>
-      </v-card>
+        </div>
+      </div>
     </v-flex>
   </v-layout>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
-export default {
-  components: {
-    Logo,
-    VuetifyLogo
+<style scoped lang="scss">
+.contents-container{
+  & p, h1{
+    text-align: center;
+    color: var(--v-font-base);
+  }
+  & div{
+    margin-top: 50px;
+  }
+  & h1{
+    margin: 50px 0;
   }
 }
-</script>
+</style>

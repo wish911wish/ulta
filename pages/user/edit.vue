c2dd2075d88ad68784182c9858b8f46344ed978e
wish911wish <wish911wish@gmail.com>
2019-12-19 06:44:30 +0900

    fix user edit.vue for change class name

--- pages/user/edit.vue
+++ pages/user/edit.vue
@@ -12,8 +12,8 @@
     >
       <div class="contents-container">
         <section-large :name="'PROFILE'" />
-        <div class="profile-image icon-frame-middle mt-5">
-          <img class="icon-middle object-fit-cover" :src='iconImage' />
+        <div class="profile-image icon-frame w-h-65 mt-5">
+          <img class="object-fit-cover w-100" :src='iconImage' />
         </div>
         <v-file-input @change="getFileContent" label="Image"></v-file-input>
         <v-text-field
